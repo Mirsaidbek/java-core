85e79ef50b74b99d89560d5fd151327335eb1860
Mirsaid <saidsaidov00001@gmail.com>
2025-10-22 11:08:28 +0500

    task2 created

--- src/main/java/dev/said/Main.java
+++ src/main/java/dev/said/Main.java
@@ -2,6 +2,6 @@ package dev.said;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World 8");
     }
 }
